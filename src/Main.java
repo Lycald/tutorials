7a5d37da908c7c043e284619b4f053fc1a6014ba
Lucas Lee <boyliyunchen@gmail.com>
2020-01-30 23:28:08 -0500

    Change the third print function.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args){
         System.out.print("Hello World!");
         System.out.print("I love coding!");
-        System.out.print("I love you!");
+        System.out.print("I love you.");
     }
 }
